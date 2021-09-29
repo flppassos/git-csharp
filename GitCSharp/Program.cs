4f9a018c4530a0e0b6ed8963988004f6897a9f15
Felipe Passos <felipepassos08@gmail.com>
2021-09-29 13:48:31 -0300

    Alterando commits

--- GitCSharp/Program.cs
+++ GitCSharp/Program.cs
@@ -5,7 +5,7 @@ namespace GitCSharp {
         static void Main(string[] args) {
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Treinando o Git e GitHub!");
-            
+            Console.WriteLine("Deletando e atualizando commits.");
 
             Console.ReadLine();
         }
